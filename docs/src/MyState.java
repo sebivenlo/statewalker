eb4adb989960e2b14ddc421fc0b845091b0cc68f
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2017-11-15 12:10:21 +0100

    added device

--- docs/src/MyState.java
+++ docs/src/MyState.java
@@ -4,11 +4,28 @@ enum MyStates extends State {
     },
     S2{
 	...
-    };
+    },
+    S33{
+	
+        @Override
+        public void enter(Context ctx) {
+            ctx.getDevice().heater(true); //<1>
+        }
+
+        @Override
+        public void exit(Context ctx) {
+            ctx.getDevice().heater(false); //<2>
+        }
+
+        @Override
+        public void e10(Context ctx) {
+            ctx.changeFromToState("e10", this, S31);//<3>
+        }
+    }
     NULL{// handlers for all events with empty bodies
 	void e1(Context ctx){}
 	...
 
-    },
+	    };
 }
 
